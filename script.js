5d31c98127d2be7927cc61320617082925dfe68e
greene80501 <152511573+greene80501@users.noreply.github.com>
2024-08-23 18:48:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+tsParticles.load("tsparticles", {
+    "background": {
+        "color": {
+            "value": "#000000"
+        }
+    },
+    "fullScreen": {
+        "enable": true
+    },
+    "particles": {
+        "number": {
+            "value": 8000
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 0.5
+        },
+        "size": {
+            "value": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "outModes": {
+                "default": "out"
+            }
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onHover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
+});
